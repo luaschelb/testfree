4a8cf1e9c1f28a3b8bcb417bf4e871ce49cf5963
Luana Schelb <lua.schelb@gmail.com>
2024-10-09 20:27:32 -0300

    Integrado tela de edição de cenarios com a api

--- app/src/screens/TestScenarioScreen/TestScenarioEditScreen.tsx
+++ app/src/screens/TestScenarioScreen/TestScenarioEditScreen.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import TestScenario from "../../models/TestScenario";
 import TestScenarioMenuControlEnum from "../../enums/TestScenarioMenuControlEnum";
+import { useGlobalSelectedProject } from "../../context/GlobalSelectedProjectContext";
+import TestScenarioService from "../../services/TestScenarioService";
 
 
 const TestScenarioEditScreen = (props: {
@@ -8,25 +10,42 @@ const TestScenarioEditScreen = (props: {
         SetShouldUpdate: (arg: boolean) => void,
         SetMenuToShow: (arg: TestScenarioMenuControlEnum) => void
     }) => {
-    const [test_id, setTest_id] = useState("");
+    const [test_id, setTest_id] = useState(0);
     const [name, setName] = useState("");
+    const [initialName, setInitialName] = useState("");
     const [description, setDescription] = useState("");
+    const { selectedProject } = useGlobalSelectedProject();
      
     useEffect(() => {
-        setTest_id(props.lastClicked.test_id)
+        setTest_id(props.lastClicked.id)
         setName(props.lastClicked.name)
         setDescription(props.lastClicked.description)
+        setInitialName(props.lastClicked.name)
     }, [props.lastClicked])
 
-    const handleUpdateScenarioClick = (id: number, testProjectId : number) => {
-        alert("não implementado")
-        props.SetShouldUpdate(true)
+    async function handleUpdateScenarioClick(id: number, testProjectId : number) {
+        if ( !name || !description) {
+            alert('Todos os campos são obrigatórios.');
+            return;
+        }
+        try {
+            await TestScenarioService.updateTestScenario(props.lastClicked.id,{
+                name: name,
+                description: description,
+                test_project_id: selectedProject,
+            })
+            setInitialName(name)
+            alert("Sucesso")
+            props.SetShouldUpdate(true)
+        } catch (error) {
+            alert('Erro ao editar cenário: ' + (error as Error).message);
+        }
     }
     
     return (
         <div style={{display: "flex", "flexDirection": "column"}}>
             <button onClick={()=> {props.SetMenuToShow(TestScenarioMenuControlEnum.CREATE_TEST_CASE)}}>Criar caso de Teste</button>
-            <h3>{props.lastClicked.test_id} - {props.lastClicked.name}</h3>
+            <h3>{props.lastClicked.test_id} - {initialName}</h3>
             name: <input 
                     id="TestScenarioName" 
                     value={name}
