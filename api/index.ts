12664ed66d83ae44124e63d991d29b38c1a710ce
luaschelb <lua.schelb@gmail.com>
2025-09-06 20:56:28 -0300

    uncommented routes in index.ts

--- api/index.ts
+++ api/index.ts
@@ -5,10 +5,10 @@ const TestCaseController = require("./controllers/TestCaseController")
 const TestScenarioController = require("./controllers/TestScenarioController")
 const TestProjectController = require("./controllers/TestProjectController")
 const TestBuildController = require("./controllers/TestBuildController")
-// const TestPlanController = require("./controllers/TestPlanController")
-// const TestExecutionController = require("./controllers/TestExecutionController")
-// const TestExecutionTestCaseController = require("./controllers/TestExecutionTestCaseController")
-// const TestFileController = require("./controllers/TestFileController")
+const TestPlanController = require("./controllers/TestPlanController")
+const TestExecutionController = require("./controllers/TestExecutionController")
+const TestExecutionTestCaseController = require("./controllers/TestExecutionTestCaseController")
+const TestFileController = require("./controllers/TestFileController")
 
 const app = express();
 
@@ -19,10 +19,10 @@ app.use('/projetos', TestProjectController);
 app.use('/scenarios', TestScenarioController);
 app.use('/testcases', TestCaseController);
 app.use('/builds', TestBuildController);
-// app.use('/test-plans', TestPlanController);
-// app.use('/executions', TestExecutionController);
-// app.use('/testexecutions_testcases', TestExecutionTestCaseController);
-// app.use('/testfiles', TestFileController);
+app.use('/test-plans', TestPlanController);
+app.use('/executions', TestExecutionController);
+app.use('/testexecutions_testcases', TestExecutionTestCaseController);
+app.use('/testfiles', TestFileController);
 
 const PORT = 8080;
 app.listen(PORT, () => {
