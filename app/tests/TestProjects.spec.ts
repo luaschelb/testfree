b3c31d8ca891fde5fe6299dbddccb31906af8a94
luaschelb <lua.schelb@gmail.com>
2025-09-24 20:47:58 -0300

    Cleaned some lines in TestProjectTests

--- app/tests/TestProjects.spec.ts
+++ app/tests/TestProjects.spec.ts
@@ -10,32 +10,27 @@ test('Adicionar projeto', async ({ page }) => {
     request.url() === process.env.REACT_APP_API_BASE_URL + '/projetos' && request.method() === 'POST',
   )
   await page.getByTestId("buttonRegisterProject").click()
-  const response = (await (await request).response())
+  const response = await (await request).response()
   expect(response?.status()).toBe(201)
   const responseJson = await response?.json()
   await page.waitForURL('**/projetos')
-  // check if project created in is table
-  const cellWithNewEntry = await page.locator("tbody > tr:last-child > td").all()
-  expect(cellWithNewEntry[0]).toHaveText(`${responseJson.id}`)
-  expect(cellWithNewEntry[1]).toHaveText(`${responseJson.name}`)
-  expect(cellWithNewEntry[2]).toHaveText(`${responseJson.description}`)
+  const cellWithNewEntry = await page.locator("tbody > tr:last-child > td")
+  expect(cellWithNewEntry.nth(0)).toHaveText(`${responseJson.id}`)
+  expect(cellWithNewEntry.nth(1)).toHaveText(`${responseJson.name}`)
+  expect(cellWithNewEntry.nth(2)).toHaveText(`${responseJson.description}`)
 })
 
 test("Deletar projeto", async ({page}) => {
   await page.goto('projetos')
   // wait for at least of valid row to be rendered
   await expect(await page.getByLabel('Editar').count()).toBeGreaterThan(0)
-  // rest of the test
   const lengthBeforeDelete = await page.getByLabel('Deletar').count()
   const request = page.waitForRequest(request =>
     request.url().startsWith(process.env.REACT_APP_API_BASE_URL + '/projetos/') && request.method() === 'DELETE',
   )
-  // prepara o listener para aceitar o confirm
-  page.once('dialog', dialog => {
-    dialog.accept()
-  });
+  page.once('dialog', dialog => { dialog.accept() });
   await page.getByLabel('Deletar').first().click();
-  const response = (await (await request).response())
+  const response = await (await request).response()
 
   await expect(page.getByLabel('Deletar')).toHaveCount(lengthBeforeDelete - 1);
 })
